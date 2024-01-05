57bdd95b87c0e625f4fa109ade4c69364e22b7e5
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-05 16:00:37 +0530

    chore : initial server setup

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server Listening on Port ${PORT}`);
+});
