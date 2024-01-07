cad6f3c2f147b644e35b81df5e747071d04ce844
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-07 17:11:01 +0530

    feat: implement uploadFile function

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,9 +5,22 @@ export const getSignedUrl = async () => {
   try {
     const response = await axios.get(`${API_URL}/image-url`);
     return response.data;
-} catch (error) {
-    console.log('Error while calling the API ', error.message);
+  } catch (error) {
+    console.log("Error while calling the API ", error.message);
     return error.response.data;
-}
-  };
-  
+  }
+};
+
+const headers = {
+  "Content-Type": "multipart/form-data",
+};
+
+export const uploadFile = async (url, file) => {
+  try {
+    const response = await axios.put(url, file, { headers: headers });
+    return response.data;
+  } catch (error) {
+    console.log("Error while calling the API ", error.message);
+    return error.response.data;
+  }
+};
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { getSignedUrl } from "../../services/api";
+import { getSignedUrl, uploadFile } from "../../services/api";
 function Home() {
   const [url, setUrl] = useState("");
   const [file, setFile] = useState("");
@@ -15,56 +15,76 @@ function Home() {
     getData();
   }, []);
 
+  useEffect(() => {
+    const getData = async () => {
+      const response = await uploadFile(url, file);
+      setUrl(url.split("?")[0]);
+    };
+    file && getData();
+  }, [file]);
+
   return (
-    <div className="h-screen">
-      <div className=" mt-20 lg:w-[900px] md:w-[400px] bg-slate-200 mx-auto  ">
-        <div className="card-body p-10">
-          <h1 className="text-2xl font-bold">File Buddy</h1>
-          <p className="mt-3">
-            Convenient file sharing in three steps without registration.
-          </p>
-          <p className="mt-3">
-            {" "}
-            <span className="me-2">1.</span>{" "}
-            <input
-              type="file"
-              name=""
-              id=""
-              className="hidden"
-              ref={fileInputRef}
-              onChange={(e) => {
-                setFile(e.target.files[0]);
-              }}
-            />
-            <button
-              onClick={() => fileInputRef.current.click()}
-              className="px-5 bg-blue-600 text-white rounded-lg py-2 me-2"
-            >
+    <>
+      <div className="h-screen">
+        <div className=" mt-10 lg:w-[900px]  bg-slate-200 mx-auto  ">
+          <div className="card-body p-10">
+            <h1 className="text-2xl font-bold">File Buddy</h1>
+            <p className="mt-3">
+              Convenient file sharing in three steps without registration.
+            </p>
+            <p className="mt-3">
               {" "}
-              Select to File Upload
-            </button>
-            or drag-and-drop files into this browser window.
-          </p>
-          <p className="mt-3">
-            {" "}
-            <span className="me-2">2.</span> Wait Untile The file upload.
-          </p>
-          <p className="mt-3">
-            {" "}
-            <span className="me-2">3.</span> The files will be available at{" "}
-            <a className="underline text-blue-700" href={url.split("?")[0]}>
-              {url.split("?")[0]}
-            </a>{" "}
-            which is a link you can share.
-          </p>
-          <p className="mt-10">
-            The file uploads will cancel if you move away from this page before
-            they complete. Uploaded files can be deleted manually at any time
-            and will in any case be deleted automatically 6 days from now.
-          </p>
+              <span className="me-2">1.</span>{" "}
+              <input
+                type="file"
+                name=""
+                id=""
+                className="hidden"
+                ref={fileInputRef}
+                onChange={(e) => {
+                  setFile(e.target.files[0]);
+                }}
+              />
+              <button
+                onClick={() => fileInputRef.current.click()}
+                className="px-5 bg-blue-600 text-white rounded-lg py-2 me-2"
+              >
+                {" "}
+                Select to File Upload
+              </button>
+              or drag-and-drop files into this browser window.
+            </p>
+            <p className="mt-3">
+              {" "}
+              <span className="me-2">2.</span> Wait Untile The file upload.
+            </p>
+            <p className="mt-3">
+              <span className="me-2">3.</span>
+              The files will be available at{" "}
+              <a className="underline text-blue-700" href={url.split("?")[0]}>
+                {url.split("?")[0]}
+              </a>
+              which is a link you can share.
+            </p>
+            <p className="mt-10">
+              The file uploads will cancel if you move away from this page
+              before they complete. Uploaded files can be deleted manually at
+              any time and will in any case be deleted automatically 6 days from
+              now.
+            </p>
+            <div className="w-fit bg-gray-300 mt-10 p-2 rounded-lg">
+              <p className="text-center">Preview</p>
+              {file && (
+                <img src={url} alt="image" className="w-48 p-3 rounded-lg" />
+              )}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="footer bg-gray-800 py-20">
+<h1 className="text-center text-white text-xl underline "><a target="_blank" href="https://github.com/saurabhjaykar1603">Developed By Saurabh</a></h1>
+      </div>
+    </>
   );
 }
 
