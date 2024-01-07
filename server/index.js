bbaa673c7488d09fbfc79a361400c254a2181de1
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-07 17:12:33 +0530

    full stack hosting

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
-import express from 'express';
-import routes from './routes/route.js'
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import routes from "./routes/route.js";
+import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import path from "path";
+
+const __dirname = path.resolve();
 dotenv.config();
 const app = express();
 
-// app.use(express.json());  
+// app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
-app.use('/', routes);
+app.use("/", routes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server Listening on Port  ${PORT}`);
